226d814cb1c127a7fd74536ab6e8dcab14c2dfc1
sergii <sergiishevchenko017@gmail.com>
2018-11-24 11:32:57 +0300

    first commit

--- entries/views.py
+++ entries/views.py
@@ -3,12 +3,13 @@ from .models import Entry
 
 
 def index(request):
-    return render(request, 'entries/index.html')
-
-
-def add(request):
     entries = Entry.objects.all()
 
     context = {'entries': entries}
 
-    return render(request, 'entries/add.html', context)
+    return render(request, 'entries/index.html', context)
+
+
+def add(request):
+
+    return render(request, 'entries/add.html')
